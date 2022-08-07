0a889795c95e00b4f0dd2febdbce3c72b9c5c42b
redyouzi <1589029587@qq.com>
2022-08-07 11:09:01 +0800

    java review-day02

---
+++ src/day02/Student.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Student {
+    String name;
+    char gender;
+    String hobby;
+
+    public void study() {
+        System.out.println("名称:" + name + "性别:" + gender + "爱好:" + hobby + "的学生开始学习了");
+    }
+}
---
+++ src/day02/Test01.java
@@ -0,0 +1,25 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//两个对象内存分析
+public class Test01 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.name = "宝马";
+        car1.price = 50.88;
+        System.out.println(car1.name);
+        System.out.println(car1.price);
+        car1.start();
+        car1.run();
+
+        Car car2 = new Car();
+        car2.name = "奔驰";
+        car2.price = 60;
+        System.out.println(car2.name);
+        System.out.println(car2.price);
+        car2.start();
+        car2.run();
+    }
+}
